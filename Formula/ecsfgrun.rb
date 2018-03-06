0cacd7ae65d1f8386e9248d6ec8bf88738555c37
goreleaserbot <goreleaser@carlosbecker.com>
2018-03-06 23:06:58 +0900

    ecsfgrun version v0.2.0

--- Formula/ecsfgrun.rb
+++ Formula/ecsfgrun.rb
@@ -1,9 +1,9 @@
 class Ecsfgrun < Formula
   desc "AWS assume role credential wrapper"
   homepage "https://github.com/masahide/ecsfgrun"
-  url "https://github.com/masahide/ecsfgrun/releases/download/v0.1.0/ecsfgrun_Darwin_x86_64.tar.gz"
-  version "0.1.0"
-  sha256 "1ff7ad71d900e90e29c4cadd4930d253a09392702af53731d71ecca6a2e50318"
+  url "https://github.com/masahide/ecsfgrun/releases/download/v0.2.0/ecsfgrun_Darwin_x86_64.tar.gz"
+  version "0.2.0"
+  sha256 "37c3df421d85ef881203d9b8a3428e179b6fd3580c4b2b45a3d6a6a60b5372c7"
 
   def install
     bin.install "ecsfgrun"
