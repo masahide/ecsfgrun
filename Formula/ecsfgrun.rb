015e0918d471037bddad14da961265596e548049
goreleaserbot <goreleaser@carlosbecker.com>
2018-03-20 20:03:02 +0900

    Brew formula update for ecsfgrun version v0.4.0

--- Formula/ecsfgrun.rb
+++ Formula/ecsfgrun.rb
@@ -1,9 +1,9 @@
 class Ecsfgrun < Formula
   desc "AWS assume role credential wrapper"
   homepage "https://github.com/masahide/ecsfgrun"
-  url "https://github.com/masahide/ecsfgrun/releases/download/v0.3.0/ecsfgrun_Darwin_x86_64.tar.gz"
-  version "0.3.0"
-  sha256 "5e9beda2b4af5798433853f9b1a03dec8e3ef05d8a32f6833b31fcb49ce83cb0"
+  url "https://github.com/masahide/ecsfgrun/releases/download/v0.4.0/ecsfgrun_Darwin_x86_64.tar.gz"
+  version "0.4.0"
+  sha256 "ade64be60b7b6f7ac2f7422ced4e0f4add6c2a2dd7aa230681f9c399b4493915"
 
   def install
     bin.install "ecsfgrun"
